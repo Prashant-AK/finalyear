56e44a1e829745672c017153492cfebf2cd9fa24
Prashant-AK <ps.prashantsingh.98@gmail.com>
2021-07-16 09:08:34 +0530

    update 2

--- src/Components/admin/Customer.js
+++ src/Components/admin/Customer.js
@@ -1,57 +1,47 @@
 import React, { useState, useEffect } from "react";
 import "./AdminCont.css";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
 function Customer() {
+  const classes = useStyles();
   return (
     <div className="admin_space">
       <h1>Customer Information</h1>
       <div className="container customer_cont">
-        <table className="table table-bordered ">
-          <tr>
-            <th>Sr No.</th>
-            <th>Name</th>
-            <th>Mob No.</th>
-            <th>E-mail</th>
-          </tr>
-
-          {/* {Object.keys(quries)
-            .map((key, index) => (
-              <tr key={key}>
-                <td>{index + 1}</td>
-                <td>{quries[key].fname}</td>
-                <td>{quries[key].phno}</td>
-                <td>{quries[key].email}</td>
-                <td>
-                  <p>{quries[key].querry}</p>
-                </td>
-              </tr>
-
-            ))
-            .reverse()} */}
-          <tr>
-            <td>1</td>
-            <td>Prashant</td>
-            <td>1234567890</td>
-            <td>ak@x.com</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Yash Kushwah</td>
-            <td>1234598798</td>
-            <td>yash@gmail.com</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Prakhar</td>
-            <td>1234567890</td>
-            <td>prakhar@gmail.com</td>
-          </tr>
-          <tr>
-            <td>4</td>
-            <td>Anas Husain</td>
-            <td>12765768798</td>
-            <td>anas@gmail.com</td>
-          </tr>
-        </table>
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Sr No.</TableCell>
+                <TableCell align="left">Name</TableCell>
+                <TableCell align="left">Mob No.</TableCell>
+                <TableCell align="left">E-mail</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell component="th" scope="row">
+                  1
+                </TableCell>
+                <TableCell align="left">Prashant</TableCell>
+                <TableCell align="left">23456789</TableCell>
+                <TableCell align="left">ak@x.com</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
     </div>
   );
